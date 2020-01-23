954acc202dec9e0d56de34d58529d151e5b1c619
Yasantha Kalupahana <yasa.kalupahana@gmail.com>
2020-01-23 16:08:52 +1100

    git commit -m 'adding changes to the site'

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
-imports:[MatCardModule,MatDividerModule,MatProgressBarModule],
-exports: [MatCardModule,MatDividerModule,MatProgressBarModule]
+imports:[MatCardModule,MatDividerModule,MatProgressBarModule,MatButtonModule],
+exports: [MatCardModule,MatDividerModule,MatProgressBarModule,MatButtonModule]
 })
 
 export class MaterailModule {}
